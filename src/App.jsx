303f34eb19840f6888224465c347ae8911efce14
syunsuke <s.syun617@icloud.com>
2025-12-06 08:00:07 +0900

    ✨material-uiを使ってコンポーネントをつくる

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { CssBaseline } from "@mui/material";
+import TodoList from "./TodoList";
 
 const App = () => {
   return (
     <div>
       <CssBaseline />
       <h1>Todos</h1>
+      <TodoList />
     </div>
   );
 };
