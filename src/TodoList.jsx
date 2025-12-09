e201673102405e737e87ae4245b46bacfc351290
syunsuke <s.syun617@icloud.com>
2025-12-09 23:40:08 +0900

    ✨todo一覧の表示と削除機能

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -58,25 +58,3 @@ const TodoList = () => {
 };
 
 export default TodoList;
-
-// export default function CheckboxList() {
-//   const [checked, setChecked] = React.useState([0]);
-
-//   const handleToggle = (value) => () => {
-//     const currentIndex = checked.indexOf(value);
-//     const newChecked = [...checked];
-
-//     if (currentIndex === -1) {
-//       newChecked.push(value);
-//     } else {
-//       newChecked.splice(currentIndex, 1);
-//     }
-
-//     setChecked(newChecked);
-//   };
-
-//   return (
-//     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-//       {[0, 1, 2, 3].map((value) => {
-
-// }
