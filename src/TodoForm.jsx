46ad063e92d3b56dbc4068e5a531e61c0b636217
syunsuke <s.syun617@icloud.com>
2025-12-10 09:25:26 +0900

    🩹valueの記述漏れを修正

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -14,6 +14,7 @@ const TodoForm = () => {
         <TextField
           onChange={handleChange}
           id="outlined-basic"
+          value={text}
           label="Outlined"
           variant="outlined"
           slotProps={{
