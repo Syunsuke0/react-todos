1ce2b49ccc3bf80aa4a1356ac60eb2fa9edb86ec
syunsuke <s.syun617@icloud.com>
2025-12-10 09:23:39 +0900

    ✨todo追加フォームの見た目の部分を実装

---
+++ src/TodoForm.jsx
@@ -0,0 +1,36 @@
+import { Create } from "@mui/icons-material";
+import { IconButton, InputAdornment, ListItem, TextField } from "@mui/material";
+import { useState } from "react";
+
+const TodoForm = () => {
+  const [text, setText] = useState("");
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <ListItem>
+      <form>
+        <TextField
+          onChange={handleChange}
+          id="outlined-basic"
+          label="Outlined"
+          variant="outlined"
+          slotProps={{
+            input: {
+              endAdornment: (
+                <InputAdornment position="start">
+                  <IconButton aria-label="create todo" edge="end">
+                    <Create />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            },
+          }}
+        />
+      </form>
+    </ListItem>
+  );
+};
+
+export default TodoForm;
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -8,6 +8,8 @@ import Checkbox from "@mui/material/Checkbox";
 import IconButton from "@mui/material/IconButton";
 import CommentIcon from "@mui/icons-material/Comment";
 import { useState } from "react";
+import TodoItem from "./TodoItem";
+import TodoForm from "./TodoForm";
 
 const initialTodos = [
   { id: 1, text: "とらじろうの散歩", completed: false },
@@ -21,38 +23,15 @@ const TodoList = () => {
   const [todos, setTodos] = useState(initialTodos);
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      {todos.map((todo) => {
-        const labelId = `checkbox-list-label-${todo.id}`;
-
-        return (
-          <ListItem
-            key={todo.id}
-            secondaryAction={
-              <IconButton edge="end" aria-label="comments">
-                <CommentIcon />
-              </IconButton>
-            }
-            disablePadding
-          >
-            <ListItemButton
-              role={undefined}
-              // onClick={handleToggle(value)}
-              dense
-            >
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={todo.completed}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ "aria-labelledby": labelId }}
-                />
-              </ListItemIcon>
-              <ListItemText id={labelId} primary={todo.text} />
-            </ListItemButton>
-          </ListItem>
-        );
-      })}
+      {todos.map((todo) => (
+        <TodoItem
+          todo={todo}
+          key={todo.id}
+          remove={removeTodo}
+          toggleTodo={() => toggleTodo(todo.id)}
+        />
+      ))}
+      <TodoForm />
     </List>
   );
 };
